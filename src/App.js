9cc7a5e4155bed0c5f2ba56b58bc58cbb32268bd
Srinivas <srinivasdsri1989@gmail.com>
2022-12-07 22:34:32 +0530

    msg

--- src/App.js
+++ src/App.js
@@ -1,25 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState, useEffect} from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const App=()=>{
+
+  const storedHeight =JSON.parse(localStorage.getItem('height'))
+  const storedWeight = JSON.parse(localStorage.getItem('weight'))
+  console.log(storedHeight)
+
+  const [weight, setWeight]= useState(storedWeight ===null ? 60 : storedWeight)
+  const [height, setHeight]= useState(storedHeight ===null ? 170 : storedHeight)
+
+  const getBmi =(height, weight)=>{
+    const heightInMeters = height/100
+    const bmi = weight/heightInMeters **2
+    return bmi.toFixed(2)
+  }
+
+useEffect(()=>{
+  document.title=`Your BMI: ${getBmi(height, weight)}`
+  console.log('Browser title updated')
+  
+
+}, [height, weight])
+
+
+useEffect(()=>{
+  
+  localStorage.setItem("height", JSON.stringify(height))
+  console.log('height updated in localstorage')
+
+  
+
+}, [height])
+
+useEffect(()=>{
+  
+  localStorage.setItem("weight", JSON.stringify(weight))
+  console.log('weight updated in localstorage')
+
+}, [weight])
+
+const increaseWeight=()=>{
+  setWeight(preState=>preState+1)
+
 }
 
-export default App;
+const decreaseWeight=()=>{
+  setWeight(preState=>preState-1)
+
+}
+
+const increaseheight=()=>{
+  setHeight(preState=>preState+1)
+
+}
+
+const decreaseheight=()=>{
+  setHeight(preState=>preState-1)
+
+}
+  return (
+    <>
+    <h1> Bmi calculator</h1>
+    <div>
+      <p>Weight: {weight}</p>
+      <button onClick={increaseWeight}>+</button>
+      <button onClick={decreaseWeight}>-</button>
+    </div>
+    <div>
+      <p>{height}</p>
+      <button onClick={increaseheight}>+</button>
+      <button onClick={decreaseheight}>-</button>
+    </div>
+
+    <h1>bmi:{getBmi(height, weight)}</h1>
+
+    </>
+  )
+  
+
+}
+
+export default App
